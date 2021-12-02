43b5f29800fff8e63862b45f0a38f2ab34e91436
Briankaiserx <Briankaiser@mail.ru>
2021-12-02 21:40:56 +0400

    second

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,27 @@
+
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+
+@Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Prints the checksum (MD5 by default) of a file to STDOUT.")
+class App {
+
+    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private File file;
+
+    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+    private String algorithm = "MD5";
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        byte[] fileContents = Files.readAllBytes(file.toPath());
+        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
+        return 0;
     }
-}
 
 
