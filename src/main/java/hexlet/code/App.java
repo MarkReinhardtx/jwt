b7fae71b49e53ae74f8ab91cfc7864272dc5fd92
Briankaiserx <Briankaiser@mail.ru>
2021-12-20 23:02:04 +0400

    update

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,21 +4,34 @@ import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
 
-@Command(
-        name = "gendiff",
-        mixinStandardHelpOptions = true,
-        version = "gendiff 0.3",
-        description = "Compares two configuration files and shows a difference.")
+@CommandLine.Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Prints the checksum (MD5 by default) of a file to STDOUT.")
+class App implements Callable<Integer> {
 
-public class App {
+    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private File file;
 
-    @Option(
-            names = {"-f", "--format"},
-            description = "output format [default: stylish]",
-            required=true)
-            private String required;
-    @Parameters(index = "0", description = "path to first file")
-    private String file1;
-    @Parameters(index = "1", description = "path to second file")
-    private String file2;
+    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+    private String algorithm = "MD5";
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        byte[] fileContents = Files.readAllBytes(file.toPath());
+        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
+        return 0;
+    }
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+}
