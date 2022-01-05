2980d3f3267aea8b352f7565a1510207b486f4c6
Briankaiserx <Briankaiser@mail.ru>
2022-01-05 19:47:16 +0400

    update

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -40,4 +40,5 @@ public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
     }
+
 }
--- src/test/java/DifferTest.java
+++ src/test/java/DifferTest.java
@@ -21,25 +21,35 @@ public class DifferTest {
 
     @Test
     public void getDifferJson() throws Exception {
-        String expected = getFile("file1_file2.txt").substring(0, getFile("file1_file2.txt").length() - 1);
+        String expected = getFile("file1_file2.txt")
+                .substring(0, getFile("file1_file2.txt").length() - 1);
         String result = Differ.generate(getPath("file1.json"), getPath("file2.json"));
         Assertions.assertEquals(expected, result);
 
-        String expectedYml = getFile("file1_file2_yml.txt").substring(0, getFile("file1_file2_yml.txt").length() - 1);
+        String expectedYml = getFile("file1_file2_yml.txt")
+                .substring(0, getFile("file1_file2_yml.txt").length() - 1);
         String resultYml = Differ.generate(getPath("file1.yml"), getPath("file2.yml"));
         Assertions.assertEquals(expectedYml, resultYml);
 
-        String expected2 = getFile("file1_file2_2.txt").substring(0, getFile("file1_file2_2.txt").length() - 1);
+        String expected2 = getFile("file1_file2_2.txt")
+                .substring(0, getFile("file1_file2_2.txt").length() - 1);
         String result2 = Differ.generate(getPath("file3.json"), getPath("file4.json"));
         Assertions.assertEquals(expected2, result2);
 
-        String expected3 = getFile("file1_file2_plain.txt").substring(0, getFile("file1_file2_plain.txt").length() - 1);
+        String expected3 = getFile("file1_file2_plain.txt")
+                .substring(0, getFile("file1_file2_plain.txt").length() - 1);
         String result3 = Differ.generate(getPath("file1.json"), getPath("file2.json"), "plain");
         Assertions.assertEquals(expected3, result3);
 
-        String expected4 = getFile("file1_file2_plain2.txt").substring(0, getFile("file1_file2_plain2.txt").length() - 1);;
+        String expected4 = getFile("file1_file2_plain2.txt")
+                .substring(0, getFile("file1_file2_plain2.txt").length() - 1);
         String result4 = Differ.generate(getPath("file3.json"), getPath("file4.json"), "plain");
         Assertions.assertEquals(expected4, result4);
+
+        String expected5 = getFile("file1_file2_yml_json.txt")
+                .substring(0, getFile("file1_file2_yml_json.txt").length() - 1);
+        String result5 = Differ.generate(getPath("file1.yml"), getPath("file2.yml"), "json");
+        Assertions.assertEquals(expected5, result5);
     }
 
 }
