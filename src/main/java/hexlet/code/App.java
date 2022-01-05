d3af5c4460e8fa36e5fb0bee812df4d0f3803434
Briankaiserx <Briankaiser@mail.ru>
2022-01-05 21:36:17 +0400

    update

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,11 +5,13 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Runnable {
+public class App implements Callable<Integer> {
 
     @Parameters(index = "0", description = "path to first file")
     private String filePath1;
@@ -24,19 +26,13 @@ public class App implements Runnable {
     private String format;
 
     @Override
-    public final void run() {
-        try {
-            System.out.println(Differ.generate(filePath1, filePath2, format));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public final Integer call() throws Exception {
+        System.out.println(Differ.generate(filePath1, filePath2, format));
+        return 0;
     }
 
-    public static void main(String[] args) {
-        final var commandLineRunner = new CommandLine(new App());
-        commandLineRunner.execute(args);
-
-        final String result = commandLineRunner.getExecutionResult();
-        System.out.println(result);
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
